834755d0350e278891f3169df097a2163dedde34
Chase Hoselle <choselle@yahoo.com>
2014-12-03 00:12:08 -0600

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20141203045003) do
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password_digers"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
